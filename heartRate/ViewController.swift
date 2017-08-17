c79443bcf3961df9c4d9313d9505b4313be2d7a4
arashkashi@gmail.com <arashkashi@gmail.com>
2017-08-17 15:37:31 +0200

    Initial Commit

---
+++ heartRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heartRate
+//
+//  Created by Arash K. on 2017-08-17.
+//  Copyright © 2017 Arash K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
